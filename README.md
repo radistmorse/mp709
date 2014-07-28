4a2a61e8005e6e1f81bd5f56f99720338ddead37
Radist Morse <radist.morse@gmail.com>
2014-07-28 11:59:13 +0200

    update for hardware IDs, readme update

--- README.md
+++ README.md
@@ -24,27 +24,37 @@ Add user who works with mp709 to group "masterkit"
 USAGE
 -------------------
 
-As an output, the program lists all the mp709 devices connected to the box with the pathes
-and internal IDs (not persistent, may change after update or usb device reconnections)
+As an output, the program lists all the mp709 devices connected to the box with the pathes,
+internal IDs (not persistent, may change after software update or usb device reconnections), and unique
+hardware IDs (always the same for the same device)
 
-You can use path, internal ID or "all" to mark the device(s)
+You can issue **on** or **off** commands to the individual relays by using the path,
+internal IDs (decimal integer) or hardware IDs (hex integer), or you can use **all** to address all the devices.
 
 If the device is unresponsive there would be a message in stderr, but the program returns 0 in any case.
+
+The non-existent devices would be ignored without errors.
 
 EXAMPLES
 ----------------
 
     ./mp709 on 1 3 /dev/hidraw5 off /dev/hidraw2 4
+Switches on the devices with internal IDs 1 and 3 and the path /dev/hidraw5, and off the device with internal ID 4 and the path /dev/hidraw2.
 
-    ./mp709 off all on 2
+    ./mp709 off all on 2 0x17F
+Switches off all the devices except the ones with internal ID 2 and a hardware ID 0x17F, which are switched on instead.
 
     ./mp709 on 1 off 2 on 3 off 4
+Switches on the devices with internal IDs 1 and 3, and off the devices with internal IDs 2 and 4.
 
     ./mp709 on 2 off all
-    pointless: the commandline is parsed from left to right,
-    so "on 2" will be ignored
+Switches off all the devices. The program parses the input from left to the right, so "on 2" will be ignored.
+If the device with internal ID 2 was off, it will remain so, without switching on and off.
 
     ./mp709 on 2 off 2
-    same as before
+Switches off the device with internal ID 2. If it was off, it will remain so, without switching on and off.
 
-
+    ./mp709 on all
+    sleep 5
+    ./mp709 off all
+Hardware reboot script for the devices, connected to the NC contacts of the relays.
--- mp709.c
+++ mp709.c
@@ -23,56 +23,102 @@
 
 int main(int argc, char* argv[])
 {
+	if (argc > 1 && strcmp(argv[1], "--help") == 0) {
+		printf("Usage: %s [on|off [<path>|<decimal int>|<hex int>|all]...]...\n", argv[0]);
+		printf("\t'on' or 'off' is the command that is issued to all subsequent relays\n");
+		printf("\t<path> - determine the relay by path, usually '/dev/hidrawX'\n");
+		printf("\t<decimal int> - determine the relay by the internal number.\n");
+		printf("\t                Be aware, that this number is not persistent,\n");
+		printf("\t                and may change on device reconnection, software upgrade, or reboot.\n");
+		printf("\t<hex int> - determine the relay by a unique ID.\n");
+		printf("\t            This ID is persistent, and will always identify the same relay.\n");
+		printf("\t'all' - apply the command to all relays.\n");
+		printf("\n\tCalling without parameters will list all the devices with the corresponding paths, internal numbers and IDs.\n\n");
+		printf("Example: %s on all off /dev/hidraw2 3 0x17F\n", argv[0]);
+		printf("\tSwitches on all the relays except for three, determined by their path, internal number and unique ID respectively.\n");
+		return 0;
+	}
 	unsigned char buf[65];
-	//#define MAX_STR 255
-	//wchar_t wstr[MAX_STR];
-	hid_device *handle;
-	int i=0;
+	unsigned long device_id;
+
 	struct hid_device_info *devs, *cur_dev;
+	hid_device *handle;
+
+	int i=0;
 
 	devs = hid_enumerate(0x16c0, 0x05df);
+
+	if (devs == NULL) {
+		printf("No devices found.\n");
+		return 0;
+	}
+
 	cur_dev = devs;
 	while (cur_dev) {
 		i++;
-		//parsing input here (yes, for every device anew)
-		int arg_on_off = 0;
-		int on_off = 0;
-		for (int arg = 1; arg < argc; arg++){
-			//we look which command was the most recent
-			if(strcmp(argv[arg], "on") == 0) {
-				arg_on_off = 1;
-				continue;
-			}
-			if(strcmp(argv[arg], "off") == 0) {
-				arg_on_off = -1;
-				continue;
-			}
 
-			//"all" means that the most recent command applies to all
-			if(strcmp(argv[arg], "all") == 0) {
-				on_off = arg_on_off;
-				continue;
-			}
-
-			//it's not a command, hence it's the device
-			int device;
-			if (sscanf (argv[arg], "%i", &device)!=1) {
-				//match by path
-				if (strcmp(argv[arg], cur_dev->path) == 0)
-						on_off = arg_on_off;
-			} else {
-				//match by int
-				if (device == i)
-					on_off = arg_on_off;
-			}
-		}
-
-		//by this point we have on_off set to 0,-1 or 1
 		handle = hid_open_path(cur_dev->path);
-		char state[20] = "unknown";
 		if (handle) {
+		
+			memset(buf, sizeof(buf), 0);
+			device_id = 0;
+			buf[0] = 0x1d;
+			if (hid_send_feature_report(handle, buf, 8) > -1){
+				if (hid_get_feature_report(handle, buf, 8) > -1){
+					if (buf[0]==0x1d) {
+						device_id = (buf[4] << 24) + (buf[5] << 16) + (buf[6] << 8) + buf[7];
+						//printf("0x%lx\n", device_id);
+					}
+				}
+			}
+			
+			if (device_id == 0) {
+				fprintf(stderr, "Unable to get unique ID for the device number %d path: %s\n", i, cur_dev->path);
+			}
+			
+			//parsing input here (yes, for every device anew)
+			int arg_on_off = 0;
+			int on_off = 0;
+			for (int arg = 1; arg < argc; arg++){
+				//we look which command was the most recent
+				if(strcmp(argv[arg], "on") == 0) {
+					arg_on_off = 1;
+					continue;
+				}
+				if(strcmp(argv[arg], "off") == 0) {
+					arg_on_off = -1;
+					continue;
+				}
+	
+				//"all" means that the most recent command applies to all
+				if(strcmp(argv[arg], "all") == 0) {
+					on_off = arg_on_off;
+					continue;
+				}
+	
+				//it's not a command, hence it's the device
+				int device;
+				if (sscanf (argv[arg], "%i", &device)!=1) {
+					//match by path
+					if (strcmp(argv[arg], cur_dev->path) == 0)
+							on_off = arg_on_off;
+				} else {
+					if (argv[arg][0] == '0' && argv[arg][1] == 'x') {
+						//hex int, it's a unique id
+						if (device_id == device)
+							on_off = arg_on_off;
+					} else {
+						//decimal int, match by number
+						if (device == i)
+							on_off = arg_on_off;
+					}
+				}
+			}
+	
+			//by this point we have on_off set to 0,-1 or 1
+			char state[20] = "unknown";
 			//some magic
-			buf[0] = 0x00; // First byte is report number
+			buf[0] = 0x00;
 			buf[1] = 0x1d;
 			buf[2] = 0x00;
 			hid_send_feature_report(handle, buf, 8);
@@ -100,13 +146,16 @@ int main(int argc, char* argv[])
 				sprintf(state, "off");
 			else if (buf[2] == 0)
 				sprintf(state, "on");
-		} else {
-			if (on_off != 0) {
-				fprintf(stderr,"Unable to change the state of the device number %d path: %s\n", i++, cur_dev->path);
+				
+			hid_close(handle);
+			if (device_id) {
+				printf("Device number %d path: %s unique ID: 0x%lx state: %s\n", i, cur_dev->path, device_id, state);
+			} else {
+				printf("Device number %d path: %s state: %s\n", i, cur_dev->path, state);
 			}
+		} else {
+			fprintf(stderr, "Unable to access the device number %d path: %s\n", i, cur_dev->path);
 		}
-		hid_close(handle);
-		printf("Device number %d path: %s state: %s\n", i++, cur_dev->path, state);
 
 
 		cur_dev = cur_dev->next;
